b7046a6ab7248ebed444aac7bcc94e8a7fa5e1cf
Olek <94283587+olek-program@users.noreply.github.com>
2022-12-13 15:42:58 +0100

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,37 @@
+from random import randint
+
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+
+print("If you want to add enter + and if you want to subtract -")
+mathfunction = input("- + * or / : ")
+
+
+def addition():
+    print(a + b)
+
+
+def subtraction():
+    print(a - b)
+    
+
+def multiplication():
+    print(a * b)
+
+
+def division():
+    print(a / b)
+
+
+if mathfunction == "+":
+    addition()
+
+
+if mathfunction == "-":
+    subtraction()
+    
+if mathfunction == "*":
+    multiplication()
+    
+if mathfunction == "/":
+    division()
